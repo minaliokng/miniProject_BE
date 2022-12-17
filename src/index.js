78a07b528e4e3087b7f8e96a0218ca759e40901d
GogumaYummy <sora1211x@gmail.com>
2022-12-17 21:42:11 +0900

    나는 실패작이래...

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ const logger = require('./config/logger');
 const { PORT } = process.env;
 
 // DB에 연결
-database.connect((err) => {
-  // 에러가 존재하면 에러 로깅
+database.query('SHOW TABLES', (err, result) => {
   if (err) return logger.error(`DB 연결 실패: ${err.stack}`);
-  logger.info('DB 연결 성공');
+  else logger.info('DB 연결 성공!');
 });
 
 // 서버 실행

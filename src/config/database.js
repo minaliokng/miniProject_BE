b2300898f33d100c64eb0a53f0886e38c1fc659d
GogumaYummy <sora1211x@gmail.com>
2022-12-18 03:24:25 +0900

    mysql => mysql2로 변경

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE } = process.env;
 
-const connection = mysql.createConnection({
+const database = mysql.createPoolPromise({
   user: DB_USER,
   password: DB_PASSWORD,
   host: DB_HOST,
   database: DB_DATABASE,
 });
 
-module.exports = connection;
+module.exports = database;

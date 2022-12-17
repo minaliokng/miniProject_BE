b8a4e70aa46825c01cbe87a610a4d542b33eec78
GogumaYummy <sora1211x@gmail.com>
2022-12-18 03:53:46 +0900

    포스트 C, R

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -4,4 +4,7 @@ const PostsController = require('../controllers/posts.controller');
 const router = Router();
 const postsController = new PostsController();
 
+router.post('/', postsController.createPost);
+router.get('/', postsController.getPosts);
+
 module.exports = router;
--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -4,6 +4,13 @@ class PostsService {
   constructor() {
     this.postsRepository = new PostsRepository();
   }
+  createPost = async (post) => {
+    await this.postsRepository.createPost(post);
+  };
+
+  getPosts = async () => {
+    return await this.postsRepository.getPosts();
+  };
 }
 
 module.exports = PostsService;

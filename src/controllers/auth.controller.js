f786a2d1ac238951a38bc3adbf6a5bf1367bfff5
TaeHyeongKwon <1004kth1313@naver.com>
2022-12-19 02:25:23 +0900

    로그인, 회원가입 API작성 및 유효성검사

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,27 @@ class AuthController {
   constructor() {
     this.authService = new AuthService();
   }
+
+  //회원가입
+  register = async (req, res, next) => {
+    try {
+      const userInfo = { ...req.body };
+      await this.authService.register(userInfo);
+      return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+    } catch (err) {
+      next(err);
+    }
+  };
+  //로그인
+  userLogin = async (req, res, next) => {
+    try {
+      const userInfo = { ...req.body };
+      await this.authService.userLogin(userInfo);
+      res.status(200).json({ message: '로그인이 완료되었습니다.' });
+    } catch (err) {
+      next(err);
+    }
+  };
 }
 
 module.exports = AuthController;
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,57 @@
 const AuthRepository = require('../repositories/auth.repository');
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { ApiError } = require('../utils/apiError');
+const { JWT_SECRET_KEY } = process.env;
+const {
+  registerRequestPattern,
+  loginRequestPattern,
+  loginResponsePattern,
+} = require('../validations/auth.validation');
 class AuthService {
   constructor() {
     this.authRepository = new AuthRepository();
   }
+
+  //회원가입
+  register = async (userInfo) => {
+    const { email, nickname, password } =
+      await registerRequestPattern.validateAsync(userInfo);
+    const isExistuser = await this.authRepository.getUserByEmail({ email });
+    if (isExistuser.length !== 0)
+      throw new ApiError('이미 존재하는 이메일 입니다', 400);
+    if (isExistuser.nickname === nickname)
+      throw new ApiError('이미 존재하는 닉네임입니다', 400);
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    await this.authRepository.register({ email, nickname, encryptedPassword });
+  };
+
+  //로그인
+  userLogin = async (loginInfo) => {
+    const { email, password } = await loginRequestPattern.validateAsync(
+      loginInfo,
+    );
+    const [isExistuser] = await this.authRepository.getUserByEmail({ email });
+    if (isExistuser.length === 0)
+      throw new ApiError('이메일이 존재하지 않습니다.', 400);
+    const decipheredPassword = await bcrypt.compare(
+      password,
+      isExistuser.password,
+    );
+    console.log(decipheredPassword);
+    if (decipheredPassword !== true)
+      throw new ApiError('비밀번호가 일치하지 않습니다.', 400);
+    const token = await jwt.sign(
+      { userId: isExistuser.userId },
+      JWT_SECRET_KEY,
+      {
+        expiresIn: '1h',
+      },
+    );
+    await loginResponsePattern.validateAsync(token);
+
+    return token;
+  };
 }
 
 module.exports = AuthService;

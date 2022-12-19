7b4d2321741c8e681dc8e9c86dcfc92dc3af6ece
TaeHyeongKwon <1004kth1313@naver.com>
2022-12-19 10:45:56 +0900

    닉네임 이메일  중복검사 API 작성 및 유효성검사 업데이트

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -15,12 +15,35 @@ class AuthController {
       next(err);
     }
   };
+
   //로그인
   userLogin = async (req, res, next) => {
     try {
       const userInfo = { ...req.body };
       await this.authService.userLogin(userInfo);
       res.status(200).json({ message: '로그인이 완료되었습니다.' });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  //이메일 중복 검사
+  checkEmail = async (req, res, next) => {
+    try {
+      const userEmail = req.body;
+      await this.authService.checkEmail(userEmail);
+      res.status(200).json({ message: '사용 가능한 이메일입니다.' });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  //닉네임 중복 검사
+  checkNickname = async (req, res, next) => {
+    try {
+      const userNickname = req.body;
+      await this.authService.checkNickname(userNickname);
+      res.status(200).json({ message: '사용 가능한 닉네임입니다.' });
     } catch (err) {
       next(err);
     }

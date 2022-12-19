deb5e320697a04ac345d2da4b8665ddc36bba4bb
TaeHyeongKwon <1004kth1313@naver.com>
2022-12-19 18:35:29 +0900

    로그인 인증 미들웨어 내용 추가, 이메일_닉네임 중복검사 내용변경

--- src/middlewares/auth.mddleware.js
+++ src/middlewares/auth.mddleware.js
@@ -4,14 +4,15 @@ const { JWT_SECRET_KEY } = process.env;
 
 //로그인이 안된 유저가 접근시 에러발생
 const loggedInYet = async (req, res, next) => {
-  const { authorization } = req.headers;
-  const [tokenType, tokenValue] = authorization.split(' ');
   try {
-    if (tokenType === Bearer || tokenValue)
-      res.locals.user = jwt.verify(tokenValue, JWT_SECRET_KEY);
+    const { authorization } = req.headers;
+    if (!authorization) throw err;
+    const [tokenType, tokenValue] = authorization.split(' ');
+    if (tokenType === 'Bearer' || tokenValue)
+      res.locals.userId = jwt.verify(tokenValue, JWT_SECRET_KEY).userId;
     next();
   } catch (err) {
-    throw new ApiError('로그인 후 이용 가능한 기능입니다.', 400);
+    next(new ApiError('로그인 후 이용 가능한 기능입니다.', 400));
   }
 };
 
@@ -21,12 +22,29 @@ const alreadyLoggedIn = async (req, res, next) => {
     const { authorization } = req.headers;
     if (!authorization) return next();
     const [tokenType, tokenValue] = authorization.split(' ');
-    if (tokenType === Bearer || tokenValue)
+    if (tokenType === 'Bearer' || tokenValue) {
       jwt.verify(tokenValue, JWT_SECRET_KEY);
-    throw new ApiError('이미 로그인이 되어있습니다', 400);
+      next(new ApiError('이미 로그인이 되어있습니다', 400));
+    }
+    next();
   } catch (err) {
-    throw new ApiError('로그인이 유효하지 않습니다.', 400);
+    next();
   }
 };
 
-module.exports = { loggedInYet, alreadyLoggedIn };
+//로그인이 안되있어도 통과
+const passLogin = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const [tokenType, tokenValue] = authorization.split(' ');
+      if (tokenType === 'Bearer' || tokenValue)
+        res.locals.userId = jwt.verify(tokenValue, JWT_SECRET_KEY).userId;
+    }
+    next();
+  } catch (err) {
+    next();
+  }
+};
+
+module.exports = { loggedInYet, alreadyLoggedIn, passLogin };

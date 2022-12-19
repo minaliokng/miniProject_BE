8ab1246b7b14048b8dddf8a004b3383deb7ea964
minalikong <minalikong@gmail.com>
2022-12-19 18:27:54 +0900

    like 관련 기능 잘못된 uri 수정

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,13 +6,17 @@ class LikeController {
   }
 
   saveLike = async(req, res, next) => {
-    const result = await this.likeService.saveLike(req.body);
+    const postId = req.params;
+    const userId = req.body
+    const result = await this.likeService.saveLike(postId, userId);
     const messageType = Object.keys(result)[0];
     res.status(result.code).json({messageType: result[messageType]});
   }
 
   cancleLike = async(req, res, next) => {
-    const result = await this.likeService.cancleLike(req.body);
+    const postId = req.params;
+    const userId = req.body
+    const result = await this.likeService.cancleLike(postId, userId);
     const messageType = Object.keys(result)[0];
     res.status(result.code).json({messageType: result[messageType]});
   }

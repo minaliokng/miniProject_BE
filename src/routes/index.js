99d5947c1e1683d9e5fe23198de0d0beed2219b1
GogumaYummy <sora1211x@gmail.com>
2022-12-20 18:47:49 +0900

    ㅋㅋ

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const postsRouter = require('./posts.route');
 const likeRouter = require('./like.route');
 const commentsRouter = require('./comments.route');
 const rateLimiter = require('../middlewares/rateLimiter');
+const imageUploader = require('../middlewares/imageUploader');
 
 const router = Router();
 
 router.use(rateLimiter); // 요청을 제한하는 미들웨어
 
+router.post('/image', imageUploader.single('image'));
 router.get('/', (req, res) => res.json({ message: 'OK' }));
 router.use('/auth', authRouter);
 router.use('/', commentsRouter); //댓글 관련 모든 uri로

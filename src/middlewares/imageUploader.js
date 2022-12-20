1311b7d859134de6261d8aa73593a078b5b900c2
GogumaYummy <sora1211x@gmail.com>
2022-12-20 19:41:18 +0900

    cc

--- src/middlewares/imageUploader.js
+++ src/middlewares/imageUploader.js
@@ -2,18 +2,15 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const s3 = require('../config/s3');
 const postsValidation = require('../validations/posts.validation');
+const logger = require('../config/logger');
 
 module.exports = multer({
   fileFilter(req, file, callback) {
-    try {
-      // await postsValidation.createPost.input.validateAsync({
-      //   postInput: { title, content, category },
-      // });
-      postsValidation.createPost.input.validate(req.body);
-      callback(null, true);
-    } catch (err) {
-      callback(null, false);
-    }
+    const dd = postsValidation.createPost.input.validate({
+      postInput: req.body,
+    });
+    if (dd.error) logger.error(dd.error);
+    callback(null, true);
   },
   storage: multerS3({
     s3,

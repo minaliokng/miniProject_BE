5886c0fc20bfe518c4d4d1ec76e3bc145fa1bad4
TaeHyeongKwon <1004kth1313@naver.com>
2022-12-22 14:21:09 +0900

    getComments 부분에 commentId 추가

--- src/repositories/comments.repository.js
+++ src/repositories/comments.repository.js
@@ -1,59 +1,58 @@
 const database = require('../config/database');
 
 class CommentsRepository {
-  existPost = async(postId) => {
+  existPost = async (postId) => {
     const [[post]] = await database.query(
-      `SELECT * FROM Posts WHERE postId=${postId}`
+      `SELECT * FROM Posts WHERE postId=${postId}`,
     );
 
     return post;
-  }
+  };
 
-  existComment = async(commentId) => {
+  existComment = async (commentId) => {
     const [[comment]] = await database.query(
-      `SELECT * FROM Comments WHERE commentId = ${commentId}`
+      `SELECT * FROM Comments WHERE commentId = ${commentId}`,
     );
 
     return comment;
-  }
+  };
 
-  postComment = async(postId, userId, content) => {
+  postComment = async (postId, userId, content) => {
     await database.query(
       'INSERT INTO Comments(postId, userId, content) VALUES (?, ?, ?)',
       [Number(postId), userId, content],
     );
-  }
+  };
 
-  getComments = async(postId) => {
+  getComments = async (postId) => {
     const [comments] = await database.query(
       `SELECT
+        C.commentId,
         C.content,
         C.createdAt,
         C.hasUpdated,
         U.nickname AS userNickname
       FROM Comments C
       INNER JOIN Users U ON C.userId = U.userId
-      WHERE postId = ${postId}`
+      WHERE postId = ${postId}`,
     );
     return comments;
-  }
+  };
 
-  updateComment = async(commentId, content) => {
+  updateComment = async (commentId, content) => {
     await database.query(
       `UPDATE Comments
       SET
         content = ${content},
         hasUpdated = 1
       WHERE
-        commentId = ${commentId}`
+        commentId = ${commentId}`,
     );
-  }
+  };
 
-  deleteComment = async(commentId) => {
-    await database.query(
-      `DELETE FROM Comments WHERE commentId = ${commentId}`
-    );
-  }
+  deleteComment = async (commentId) => {
+    await database.query(`DELETE FROM Comments WHERE commentId = ${commentId}`);
+  };
 }
 
 module.exports = CommentsRepository;
